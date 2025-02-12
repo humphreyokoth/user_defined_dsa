4374b2fd4093e61a17a83a3846d2619795bd5d82
humphreyokoth <humpasp@gmail.com>
2025-02-12 12:42:53 +0300

    Fix doubly linked list implementation: add reverse traversal print and correct node linking

--- doublylist.py
+++ doublylist.py
@@ -21,6 +21,7 @@ class doublyLL:
     
 # Traversal of the linked list in reverse order
     def print_LL_reverse(self):
+        print()
         if self.head is None:
             print("Linked list is empty")
         else:
@@ -61,8 +62,11 @@ class doublyLL:
             while n.nref is not None:
                 n = n.nref
             n.nref = new_node
+            new_node.pref = n
 
 dl1 =   doublyLL()
+dl1.insert_empty(50)
+dl1.add_begin(10)
 dl1.print_LL()
 dl1.print_LL_reverse()
 # Doubely linked list  is a type of linked list in which each node apart from storing its data has two links. The first link points to the previous node in the linked list and the second link points to the next node in the linked list. The first node of the linked list has its previous link pointing to null similarly the last node of the linked list has its next link pointing to null.
