79354670b2a94a468ab2bbb2d7b13ccb6de20e6a
humphreyokoth <humpasp@gmail.com>
2025-02-12 14:32:34 +0300

    Add example usage for adding a node after a specified node in doubly linked list

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -108,6 +108,7 @@ class doublyLL:
 dl1 = doublyLL()
 dl1.insert_empty(50)
 dl1.add_begin(10)
+dl1.add_after(20,10)
 dl1.print_LL()
 dl1.print_LL_reverse()
 # Doubely linked list  is a type of linked list in which each node apart from storing its data has two links. The first link points to the previous node in the linked list and the second link points to the next node in the linked list. The first node of the linked list has its previous link pointing to null similarly the last node of the linked list has its next link pointing to null.
